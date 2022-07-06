dec81fc908270b848622a36f027b5eb7f6ca3833
sur1v <gsuriv@gmail.com>
2022-07-06 22:11:55 +0100

    fixing for 128x34 0.91 OLED display

--- stats.py
+++ stats.py
@@ -17,8 +17,8 @@ oled_reset = digitalio.DigitalInOut(board.D4)
 
 # Display Parameters
 WIDTH = 128
-HEIGHT = 64
-BORDER = 5
+HEIGHT = 32
+BORDER = 0
 
 # Use for I2C.
 i2c = board.I2C()
@@ -38,6 +38,7 @@ draw = ImageDraw.Draw(image)
 # Draw a white background
 draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
 
+#font = ImageFont.truetype('FixedsysExcelsiorIIIb-L2_Mono.ttf', 14)
 font = ImageFont.truetype('PixelOperator.ttf', 16)
 #font = ImageFont.load_default()
 
@@ -47,24 +48,24 @@ while True:
     draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
 
     # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
+#    cmd = "hostname -I | cut -d\' \' -f1"
+#    IP = subprocess.check_output(cmd, shell = True )
+    cmd = "top -bn1 | grep load | awk '{printf \"Load: %.2f\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell = True )
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+    cmd = "free -m | awk 'NR==2{printf \"Mem: %.0f%%\", $3*100/$2 }'"
     MemUsage = subprocess.check_output(cmd, shell = True )
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+    cmd = "df -h | awk '$NF==\"/\"{printf \"mSD: %.0f%%\", $5 }'"
     Disk = subprocess.check_output(cmd, shell = True )
     cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
     temp = subprocess.check_output(cmd, shell = True )
 
     # Pi Stats Display
-    draw.text((0, 0), "IP: " + str(IP,'utf-8'), font=font, fill=255)
-    draw.text((0, 16), str(CPU,'utf-8') + "LA", font=font, fill=255)
-    draw.text((80, 16), str(temp,'utf-8') , font=font, fill=255)
-    draw.text((0, 32), str(MemUsage,'utf-8'), font=font, fill=255)
-    draw.text((0, 48), str(Disk,'utf-8'), font=font, fill=255)
-        
+#    draw.text((0, 0), "IP: " + str(IP,'utf-8'), font=font, fill=255)
+    draw.text((0, 0), str(CPU,'utf-8') + " @", font=font, fill=255)
+    draw.text((80, 0), str(temp,'utf-8') , font=font, fill=255)
+    draw.text((0, 16), str(MemUsage,'utf-8'), font=font, fill=255)
+    draw.text((65, 16), str(Disk,'utf-8'), font=font, fill=255)
+
     # Display image
     oled.image(image)
     oled.show()
